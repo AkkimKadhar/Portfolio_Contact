1b78b938148daf56611205849f1ef637ce76673a
AkkimKadhar <akkimkadhar1@gmail.com>
2023-11-15 22:28:15 +0530

    new

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -32,12 +32,12 @@ const Contact = () => {
     <StyledContactForm>
       <form ref={form} onSubmit={sendEmail}>
         <label>Name</label>
-        <input type="text" name="from_name" />
+        <input type="text" name="from_name"  required/>
         <label>Email</label>
-        <input type="email" name="from_email" />
+        <input type="email" name="from_email" required />
         <label>Message</label>
         <textarea name="message" />
-        <input type="submit" value="Send" />
+        <input type="submit" value="Send" required />
       </form>
     </StyledContactForm>
   );
